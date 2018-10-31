9c9fa1e47f64452b3a3c6f74cf2a79490516a1f6
Åsne Stige <asnestige@gmail.com>
2018-10-31 14:03:33 +0100

    Endret ting

--- Sensob.py
+++ Sensob.py
@@ -1,3 +1,6 @@
+from reflectance_sensors import ReflectanceSensors
+from ultrasonic import Ultrasonic
+
 class Sensob:
 
     def __init__(self):
@@ -16,5 +19,34 @@ class Sensob:
             sensor.reset()
 
 
+class IRProximity_sensob(Sensob):  #FRA JONAS
+    def __init__(self):
+        super(IRProximity_sensob, self).__init__()
+        self.sensors = [IRProximitySensor()]
 
+    def update(self):
+        self.value = self.sensors[0].update()
+        print("IR", self.value)
+        return self.value
 
+class Reflectance(Sensob): #FRA JONAS
+    def __init__(self):
+        super(Reflectance, self).__init__()
+        self.sensors = [ReflectanceSensors()]
+
+    def update(self):
+        self.value = sum(self.sensors[0].update())
+        print("Reflectance", self.value)
+        return self.value
+
+class Ultrasonic_sensob(Sensob): #FRA JONAS
+    def __init__(self):
+        super(Ultrasonic_sensob, self).__init__()
+        self.sensors = [Ultrasonic()]
+
+    def update(self):
+        for sensor in self.sensors:
+            sensor.update()
+        self.value = self.sensors[0].value
+        print("Ultrasonic", self.value)
+        return self.value
--- behavior.py
+++ behavior.py
@@ -38,10 +38,74 @@ class Behavior:
 
         self.weight = self.match_degree * self.priority
 
+class Avoid_collisions(Behavior): #FRA JONAS
+    def __init__(self, distance, ir):
+        super(Avoid_collisions, self).__init__(None)
+        self.sensobs = [distance, ir]
+
+        self.priority = Config['collisionPri']
+
+    def consider_deactivation(self):
+        self.active_flag = True
+
+    def consider_activation(self):
+        self.active_flag = True
+
+    def sense_and_act(self):
+        dist = self.sensobs[0].get_value()
+
+        reflect = self.sensobs[1].get_value()
+
+        self.match_degree = 0
+        self.motor_recommendations = None
+
+        if reflect > Config['reflectThr'] or dist < Config['minDist']:
+            self.match_degree = 1
+            self.motor_recommendations = Config['J_turn']
+
+class Go(Behavior): #FRA JONAS
+    def __init__(self):
+        super(Go, self).__init__(None)
+        self.priority = Config['goPri']
+
+    def consider_activation(self):
+        self.active_flag = True
+
+    def consider_deactivation(self):
+        self.active_flag = True
+
+    def sense_and_act(self):
+        self.match_degree = 1
+        self.motor_recommendations = Config['forward']
 
 
+class StopSign(Behavior): #FRA JONAS
+    def __init__(self, camera):
+        super(StopSign, self).__init__(None)
+        self.sensobs = [camera]
 
+        self.priority = Config['stopSignPri']
 
+        self.stopped = False
 
+    def consider_deactivation(self):
+        self.active_flag = True
 
+    def consider_activation(self):
+        self.active_flag = True
 
+    def sense_and_act(self):
+        rgb = self.sensobs[0].value
+
+        self.match_degree = 0
+        self.motor_recommendations = Config['stop']
+
+        if self.stopped:
+            self.match_degree = 1
+            if rgb[1] > Config['grThr']:
+                self.match_degree = 0
+                self.stopped = False
+
+        elif rgb[0] > Config['redThr']:
+            self.stopped = True
+            self.match_degree = 1
