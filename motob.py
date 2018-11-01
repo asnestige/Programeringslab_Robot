aec3daa51b848fb8f1e1186b06d1b17922afb872
Nina Andal Aarvik <nina.andal.aarvik@gmail.com>
2018-11-01 10:37:56 +0100

    small change

--- motob.py
+++ motob.py
@@ -19,7 +19,9 @@ class Motob:
          self.values = values 
          self.operationalize() 
          
-         
+     #Motob får motor_recommendations fra Arbitrator
      #convert a motor recommendation into one or more motor settings, which are sent to the corresponding motor(s).    
      def operationalize(self):
-                 
+        motor = Motors()
+        #bruker 0.5 som duration til å begynne med, kan være at denne må endres 
+        motor.set_value(self.values, 0.5)
