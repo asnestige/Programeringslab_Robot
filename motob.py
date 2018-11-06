f9f4017e255045f903ee9b38f64b77af086645e3
Hedda Ulve <hedda.s.ulve@hotmail.com>
2018-11-06 11:23:41 +0100

    debug

--- motob.py
+++ motob.py
@@ -14,18 +14,21 @@ from motors import Motors
 class Motob: 
     
      def __init__(self,motors):
-         self.motors = motors 
+         self.motors = motors
          self.values = []
+
+
          
          
     #Receive a new motor recommendation, load it into the value slot, and operationalize it.
      def update(self, values): 
-         self.values = values 
-         self.operationalize() 
+         self.values = ''.join(values)
+         self.operationalize()
          
      #Motob får motor_recommendations fra Arbitrator
      #convert a motor recommendation into one or more motor settings, which are sent to the corresponding motor(s).    
      def operationalize(self):
         motor = Motors()
-        #bruker 0.5 som duration til å begynne med, kan være at denne må endres 
+        #bruker 0.5 som duration til å begynne med, kan være at denne må endres
+
         motor.set_value(self.values, 0.5)
